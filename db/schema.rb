45fc0228887b9d95c79b1a8ca4ea72e319b2cf0f
takashi <takashi@test.jp>
2020-10-07 07:16:42 +0000

    add kadai2

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_05_105138) do
+ActiveRecord::Schema.define(version: 2020_10_07_062807) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
   end
 
 end
